57237ef4acefabc4c77206ae63671bdaa4777364
NATTANICHA SENTHA <63010299@KMITL.AC.TH>
2020-10-08 20:25:46 +0700

    Add project files.

---
+++ Project16/Source.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int auksorn(char a[])
+{
+	int i=0,sum=0;
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a'&& a[i]<='z')
+		{
+			sum++;
+		}
+			i++;
+	}
+	return sum;
+}
+int main()
+{
+	char a[100];
+	scanf("%s",&a);
+	printf("%d", auksorn(a));
+	return 0;
+}
